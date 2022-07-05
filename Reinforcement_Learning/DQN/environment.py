04311ba08eae577c18d6562113b20f98b73b6355
oeg1n18 <oeg1n18@soton.ac.uk>
2022-07-05 11:55:15 +0100

    Converted the environment to be compatable with tf-agents library. This enables access to state of the art algorithms and distributed optimized trianing on gpu's

--- Reinforcement_Learning/DQN/environment.py
+++ Reinforcement_Learning/DQN/environment.py
@@ -1,106 +1,106 @@
+import itertools
+from abc import ABC
 
-
-from HandModel import HandModel
 import numpy as np
 import pandas as pd
-import itertools
+import tensorflow as tf
+
+import tf_agents
+from tf_agents.specs import array_spec
+from tf_agents.trajectories import time_step as ts
+from tensorflow import keras
 
 
-class hand_env(HandModel):
+class hand_env(tf_agents.environments.py_environment.PyEnvironment, ABC):
+
     def __init__(self):
         super().__init__()
-        self.step = 0
+        self._action_spec = array_spec.BoundedArraySpec(
+            shape=(), dtype=np.int64, minimum=0, maximum=61, name='action')
+        self._observation_spec = array_spec.BoundedArraySpec(
+            shape=(5,), dtype=np.float32, minimum=[0.0, 0.0, 0.0, 0.0, 0.0], name='observation')
         self.target_trajectory = np.transpose(pd.read_csv("trajectory_target.csv", header=None).to_numpy())
         self.stop_time = 2.5
         self.amp1 = 0.150
         self.amp2 = 0.10
         self.done = 0
-        self.reward = 0
-        self.channel1 = 0
-        self.channel2 = 1
-        self.state = np.array([self.stop_time,
-                               self.amp1,
-                               self.amp2,
-                               self.channel1,
-                               self.channel2]).astype(np.float32)
+        self.reward = 0.0
+        self.channel1 = 0.0
+        self.channel2 = 1.0
+        self._state = np.array([self.stop_time,
+                                self.amp1,
+                                self.amp2,
+                                self.channel1,
+                                self.channel2], dtype=np.float32)
+        self.hand_model = keras.models.load_model("Models/model_cp.h5")
+        self._episode_ended = False
 
-    def take_action(self, A):
-        old_state = np.reshape(self.state, (1, 5)).astype(np.float32)
+    def action_spec(self):
+        return self._action_spec
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    def _reset(self):
+        self._state = np.array([2.5, 0.150, 0.10, 0.0, 1.0], dtype=np.float32)
+        self._episode_ended = False
+        return ts.restart(self._state)
+
+    def _step(self, action):
+        stop_time = self._state[0]
+        amp1 = self._state[1]
+        amp2 = self._state[2]
+        chan1 = self._state[3]
+        chan2 = self._state[4]
+
+        if self._episode_ended:
+            # The last action ended the episode. Ignore the current action and start
+            # a new episode.
+            return self.reset()
+        # Make sure episodes don't go on forever.
         perms = list(itertools.permutations(np.arange(8.0), 2))
         n_AC = len(perms)
 
-        inputs = np.zeros((1, 251, 7))
-
         # take action
-        if A < n_AC:
-            self.channel1 = int(perms[A][0])
-            self.channel2 = int(perms[A][1])
-        if A == n_AC:
-            self.amp1 = self.amp1 + 0.01
-        if A == n_AC + 1:
-            self.amp1 = self.amp1 - 0.01
-        if A == n_AC + 2:
-            self.amp2 = self.amp2 + 0.01
-        if A == n_AC + 3:
-            self.amp2 = self.amp2 - 0.01
-        if A == n_AC + 4:
-            self.stop_time = self.stop_time + 0.1
-        if A == n_AC + 5:
-            self.stop_time = self.stop_time - 0.1
+        if action < n_AC:
+            chan1 = perms[action][0]
+            chan2 = perms[action][1]
+        elif action == n_AC:
+            amp1 += 0.01
+        elif action == int(n_AC + 1):
+            amp1 -= 0.01
+        elif action == int(n_AC + 2):
+            amp2 += 0.01
+        elif action == int(n_AC + 3):
+            amp2 -= 0.01
+        elif action == int(n_AC + 4):
+            stop_time += 0.1
+        elif action == int(n_AC + 5):
+            stop_time -= 0.1
+        else:
+            raise ValueError('`action` should be integer between 0 and 61.')
 
-        # Safety Checks
-        if self.stop_time > 3.0:
-            self.stop_time = 3.0
-        if self.stop_time < 1.0:
-            self.stop_time = 1.0
-        if self.amp1 > 0.15:
-            self.amp1 = 0.15
-        if self.amp2 > 0.15:
-            self.amp2 = 0.15
-        if self.amp1 < 0.025:
-            self.amp1 = 0.025
-        if self.amp2 < 0.025:
-            self.amp2 = 0.025
-
+        inputs = np.zeros((1, 251, 7))
         slope = np.arange(0, 1, 0.02 / self.stop_time)
         initial = np.zeros(np.arange(0, 0.5, 0.02).shape)
         n_slope = initial.size + slope.size
         steady_state = np.ones(251 - n_slope)
         ramp = np.hstack((initial, slope, steady_state))
 
-        if self.channel1 != 7:
-            inputs[0, :, self.channel1] = ramp * self.amp1
-        if self.channel2 != 7:
-            inputs[0, :, self.channel2] = ramp * self.amp2
+        if chan1 != 7:
+            inputs[0, :, int(chan1)] = ramp * self.amp1
+        if chan2 != 7:
+            inputs[0, :, int(chan2)] = ramp * self.amp2
+        angles = self.hand_model(inputs)
+        angles = angles.numpy()
+        error = np.sum(np.abs(angles[0, :, :] - self.target_trajectory[:, :]))
 
-        # apply input
-        self.reset_model()
-        angles = self.infer(inputs)
+        if self._episode_ended or error > 899:
+            reward = np.float32((1 / error))
+            return ts.termination(self._state, reward)
+        else:
+            self._state = np.array([stop_time, amp1, amp2, chan1, chan2], dtype=np.float32)
+            return ts.transition(self._state, reward=(1 / error), discount=0.99)
 
-        # Compute Reward
-        self.reward = -np.sum(np.abs(angles[0, :, :] - self.target_trajectory[:, :]))
-        self.reward = np.float32(self.reward)
 
-        # Compute next state
-        self.state = np.reshape(np.array([self.stop_time,
-                               self.amp1,
-                               self.amp2,
-                               self.channel1,
-                               self.channel2]), (1, 5)).astype(np.float32)
-        # Compute Done
-        return old_state, A, self.reward, self.state
-
-    def reset(self):
-        self.stop_time = 2.5
-        self.amp1 = 0.150
-        self.amp2 = 0.10
-        self.done = 0
-        self.reward = 0
-        self.channel1 = 0
-        self.channel2 = 1
-        self.state = np.array([self.stop_time,
-                               self.amp1,
-                               self.amp2,
-                               self.channel1,
-                               self.channel2])
 
--- Reinforcement_Learning/DQN/train_control_policy.py
+++ Reinforcement_Learning/DQN/train_control_policy.py
@@ -1,35 +1,165 @@
+from __future__ import absolute_import, division, print_function
 from environment import hand_env
-from DQN import DQN
-import tensorflow as tf
-from tensorflow import keras
-import csv
+import base64
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
-RL_agent = DQN()
-env = hand_env()
-rewards = []
-experience_replay_frequency = 10
-RL_agent.max_episodes = 20
+import tensorflow as tf
+from tf_agents.policies.random_tf_policy import RandomTFPolicy
+from tf_agents.eval.metric_utils import log_metrics
+import logging
+from tf_agents.utils.common import function
+from tf_agents.drivers.dynamic_step_driver import DynamicStepDriver
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.environments import ActionOffsetWrapper
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import py_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import sequential
+from tf_agents.policies import py_tf_eager_policy
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
+from tf_agents.trajectories import trajectory
+from tf_agents.specs import tensor_spec
+from tf_agents.utils import common
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-while RL_agent.episode < RL_agent.max_episodes:
+num_iterations = 20000  # @param {type:"integer"}
+initial_collect_steps = 100  # @param {type:"integer"}
+collect_steps_per_iteration = 1  # @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+batch_size = 64  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200  # @param {type:"integer"}
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 1000  # @param {type:"integer"}
 
-    state = env.state
-    action = RL_agent.get_action(state)
-    state, action, reward, nstate = env.take_action(action)
-    rewards.append(reward)
-    RL_agent.store(state, action, reward, nstate)
-    print("episode: ", RL_agent.episode, "  epsilon: ", RL_agent.epsilon, "reward: ", reward)
-    if RL_agent.episode%experience_replay_frequency == 0:
-        print("Optimizing")
-        RL_agent.experience_replay()
 
-print("Finished Training")
+# Transform the environment into a tensorflow environment
+env_train = hand_env()
+env_eval = hand_env()
+tf_env_train = tf_py_environment.TFPyEnvironment(env_train)
+tf_env_eval = tf_py_environment.TFPyEnvironment(env_eval)
 
-with open('rewards.csv', 'w', newline='') as rewards_file:
-    input_writer = csv.writer(rewards_file)
-    input_writer.writerow(rewards)
 
-policy_model = RL_agent.model
-keras.models.save_model(policy_model, "Policy_Models/policy_model.h5")
-print("model save complete")
+# Build the Qnetwork for the DQN Agent
+q_net = q_network.QNetwork(
+    tf_env_train.observation_spec(),
+    tf_env_train.action_spec(),
+    fc_layer_params=(100,))
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+train_step_counter = tf.Variable(0)
+
+
+# Create the DQN agent
+agent = dqn_agent.DqnAgent(
+    tf_env_train.time_step_spec(),
+    tf_env_train.action_spec(),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=train_step_counter)
+
+
+# Create replay buffer
+replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+    data_spec=agent.collect_data_spec,
+    batch_size=tf_env_train.batch_size,
+    max_length=100000)  # reduce if OOM error
+
+
+# Create the observer
+replay_buffer_observer = replay_buffer.add_batch
+
+
+# Second Observer to show training progress
+class ShowProgress:
+    def __init__(self, total):
+        self.counter = 0
+        self.total = total
+
+    def __call__(self, trajectory):
+        if not trajectory.is_boundary():
+            self.counter += 1
+        if self.counter % 100 == 0:
+            print("\r{}/{}".format(self.counter, self.total), end="")
+
+
+# Training metrics
+train_metrics = [
+    tf_metrics.NumberOfEpisodes(),
+    tf_metrics.EnvironmentSteps(),
+    tf_metrics.AverageReturnMetric(),
+    tf_metrics.AverageEpisodeLengthMetric(),
+]
+
+# Log the train metrics
+logging.getLogger().setLevel(logging.INFO)
+log_metrics(train_metrics)
+
+
+# Create the Collect Driver
+collect_driver = DynamicStepDriver(
+    tf_env_train,
+    agent.collect_policy,
+    observers=[replay_buffer_observer] + train_metrics)
+
+
+# Collect initial experiences with random policy
+initial_collect_policy = RandomTFPolicy(tf_env_train.time_step_spec(),
+                                        tf_env_train.action_spec())
+
+
+# Turn the replay buffer into a dataset for training
+dataset = replay_buffer.as_dataset(
+    sample_batch_size=64,
+    num_steps=2,
+    num_parallel_calls=3).prefetch(3)
+
+
+# optimize functions by converting them to tensorflow
+collect_driver.run = function(collect_driver.run)
+agent.train = function(agent.train)
+
+# Create the step driver by:
+# 1. giving it a policy to play with
+# 2. giving it a policy to collect the data with
+# 3. provide it the observers which show progress and log data in the replay buffer
+init_driver = DynamicStepDriver(
+    tf_env_train,
+    initial_collect_policy,
+    observers=[replay_buffer.add_batch, ShowProgress(200)],
+    num_steps=200)
+
+# Training Loop
+# 1. get the initial policy state
+# 2. turn the dataset into an iterator show you can get the "next" batch with "next" method
+# 3. use the collect driver to run the policy to get a time_step
+def train_agent(n_iterations):
+    time_step = None
+    policy_state = agent.collect_policy.get_initial_state(tf_env_train.batch_size)
+    iterator = iter(dataset)
+    for iteration in range(n_iterations):
+        time_step, policy_state = collect_driver.run(time_step, policy_state)
+        trajectories, buffer_info = next(iterator)
+        train_loss = agent.train(trajectories)
+        print("\r{} loss:{:.5f}".format(
+            iteration, train_loss.loss.numpy()), end="")
+        if iteration % 1000 == 0:
+            log_metrics(train_metrics)
+
+
+agent.initialize()
+final_time_step, final_policy_state = init_driver.run() # Collect some initial experiences
+train_agent(n_iterations=1000) # Run the training loop
+tf_env_train.close()
+tf_env_eval.close()
